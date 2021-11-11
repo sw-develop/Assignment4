029d6c9bdc4ad0179e005c3b1bf4dcd85804165a
shchoi94 <hyuklassic@gmail.com>
2021-11-11 23:20:06 +0900

    [feature#5] Add: deposit logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from django.db import transaction, IntegrityError
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-from accounts.models import Account
+from accounts.models import Account, TradeLog
 
 
 class AccountViewSet(viewsets.GenericViewSet):
@@ -39,7 +41,34 @@ class AccountViewSet(viewsets.GenericViewSet):
         입금
         POST /accounts/{account_id}/deposit/
         """
-        pass
+        try:
+            account = Account.objects.get(id=pk)
+        except Account.DoesNotExist:
+            return Response({"error": "account does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        if account.user != request.user:
+            return Response({"error": "Not Authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # todo: amount int value validation
+        amount = request.data.get('amount')
+        description = request.data.get('description')
+
+        if account.balance < amount:
+            return Response({"error": "잔액 부족"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            with transaction.atomic():
+                trade_log = TradeLog(
+                    amount=amount, balance=account.balance - amount, description=description,
+                    account=account, code=TradeLog.TrageCodeChoice.DEPOSIT
+                )
+                account.balance = account.balance - amount
+                trade_log.save()
+                account.save()
+        except IntegrityError:
+            # todo: 적정에러처리
+            return Response({'error': '오류발생'},status=status.HTTP_409_CONFLICT)
+        # todo: 성공 리스폰스 처리
+        return Response(status=status.HTTP_200_OK)
 
     @action(methods=['POST'], detail=True)
     def withdrawal(self, request, pk):
