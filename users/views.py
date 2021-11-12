cb1b447ce672a67406c9d973dde159411edd9332
lunayyko <youryoung.ko@gmail.com>
2021-11-12 19:08:56 +0900

    [feature#14] Add: 유저 상세 정보 조회 - UserSerializer추가 - 비밀번호, 관리자여부 제외하고 조회 - 유저 정보 조회 본인만 - RegisterUserView오타 수정

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
-from rest_framework             import serializers
+from rest_framework             import serializers, generics
 from rest_framework.generics    import CreateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework             import permissions
+from .permissions               import IsOwner
 
-from .serializers import RegisterUserSerializer
+from users.models               import User
+from .serializers               import RegisterUserSerializer, UserSerializer
 
 
-class RegisterUsertView(CreateAPIView):
+class RegisterUserView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class   = RegisterUserSerializer
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsOwner]
